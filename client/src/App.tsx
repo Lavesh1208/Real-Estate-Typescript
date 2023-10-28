1fb951b0f3bcfd4e45b22bbb80445996404b6702
Lavesh1208 <laveshs1282@gmail.com>
2023-10-28 15:42:16 +0530

    Search Page UI

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,15 +1,15 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Toaster } from 'react-hot-toast';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import PrivateRoute from './components/PrivateRoute';
+import About from './pages/About';
+import CreateListing from './pages/CreateListing';
 import Home from './pages/Home';
+import Listing from './pages/Listing';
+import Profile from './pages/Profile';
+import Search from './pages/Search';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
-import About from './pages/About';
-import Profile from './pages/Profile';
-import Header from './components/Header';
-import { Toaster } from 'react-hot-toast';
-import PrivateRoute from './components/PrivateRoute';
-import CreateListing from './pages/CreateListing';
-import UpdateListing from './pages/UpdateListing';
-import Listing from './pages/Listing';
 
 function App() {
    return (
@@ -21,6 +21,7 @@ function App() {
                <Route path="/sign-in" element={<SignIn />} />
                <Route path="/sign-up" element={<SignUp />} />
                <Route path="/about" element={<About />} />
+               <Route path="/search" element={<Search />} />
                <Route element={<PrivateRoute />}>
                   <Route path="/profile" element={<Profile />} />
                   <Route path="/create-listing" element={<CreateListing />} />
