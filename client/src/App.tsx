a1f02cdc3bb7a4593ededd9b0d8ebccdd2f1dee7
Lavesh1208 <laveshs1282@gmail.com>
2023-10-29 14:10:20 +0530

    fix update listing route

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,37 +1,42 @@
-import { Toaster } from 'react-hot-toast';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Header from './components/Header';
-import PrivateRoute from './components/PrivateRoute';
-import About from './pages/About';
-import CreateListing from './pages/CreateListing';
-import Home from './pages/Home';
-import Listing from './pages/Listing';
-import Profile from './pages/Profile';
-import Search from './pages/Search';
-import SignIn from './pages/SignIn';
-import SignUp from './pages/SignUp';
+import { Toaster } from "react-hot-toast";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import PrivateRoute from "./components/PrivateRoute";
+import About from "./pages/About";
+import CreateListing from "./pages/CreateListing";
+import Home from "./pages/Home";
+import Listing from "./pages/Listing";
+import Profile from "./pages/Profile";
+import Search from "./pages/Search";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+import UpdateListing from "./pages/UpdateListing";
 
 function App() {
-   return (
-      <>
-         <BrowserRouter>
-            <Header />
-            <Routes>
-               <Route path="/" element={<Home />} />
-               <Route path="/sign-in" element={<SignIn />} />
-               <Route path="/sign-up" element={<SignUp />} />
-               <Route path="/about" element={<About />} />
-               <Route path="/search" element={<Search />} />
-               <Route element={<PrivateRoute />}>
-                  <Route path="/profile" element={<Profile />} />
-                  <Route path="/create-listing" element={<CreateListing />} />
-                  <Route path="/listing/:listingId" element={<Listing />} />
-               </Route>
-            </Routes>
-         </BrowserRouter>
-         <Toaster />
-      </>
-   );
+	return (
+		<>
+			<BrowserRouter>
+				<Header />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/sign-in" element={<SignIn />} />
+					<Route path="/sign-up" element={<SignUp />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/search" element={<Search />} />
+					<Route element={<PrivateRoute />}>
+						<Route path="/profile" element={<Profile />} />
+						<Route path="/create-listing" element={<CreateListing />} />
+						<Route
+							path="/update-listing/:listingId"
+							element={<UpdateListing />}
+						/>
+						<Route path="/listing/:listingId" element={<Listing />} />
+					</Route>
+				</Routes>
+			</BrowserRouter>
+			<Toaster />
+		</>
+	);
 }
 
 export default App;
